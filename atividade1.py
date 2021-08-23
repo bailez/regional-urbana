aa0df371b080bed46393b5323ff62241e583036b
bailez <bailez@usp.com>
2021-08-23 09:07:32 -0300

    regional e urbana atividade 1

--- atividade1.py
+++ atividade1.py
@@ -5,6 +5,10 @@ df = pd.read_excel('dados1.xlsx', sheet_name = 'Brasil 2002')
 
 df.index = df[['COD MUN', 'Municípios/ 26 Categorias']]
 df = df.iloc[:,3:]
+regions = [ 'IND MECANICA - Indústria mecânica',
+           'ALIM E BEB - Indústria de produtos alimentícios, bebidas e álcool etílico',
+            'ADM PUBLICA - Administraçao pública direta e autárquica']
+
 # %%                 Quociente Locacional
 
 '''
@@ -42,22 +46,30 @@ sectors_total = sectoral_share.iloc[-1,:]
 
 quo_loc_sec = sectoral_share.divide(sectors_total, axis=1)
 
-# %%                  Coeficiente de Associação Geográfica
+# %%                  Coeficiente de Localização
 
-cl_reg = pd.DataFrame()
+cl = pd.DataFrame()
 
 for i in regional_share.columns:
-    cl_reg[i] = np.abs(regional_share[i] - regions_total)
+    cl[i] = np.abs(regional_share[i] - regions_total)
     
-cl_reg = cl_reg.sum(axis=0)/2
+cl = cl.sum(axis=0)/2
 
-cl_sec = pd.DataFrame()
+# %%                  Coeficiente de Associação Geográfica
 
-for i in sectoral_share.columns:
-    cl_sec[i] = np.abs(sectoral_share[i] - sectors_total)
-    
-cl_sec = cl_sec.sum(axis=0)/2
+cga = pd.DataFrame()
 
+for i in regional_share.columns:
+    for j in regional_share.columns:
+        cga[i + '//' + j] = np.abs(regional_share[i] - regional_share[j])
+
+cga = cga.sum(axis=0)/2
+cga = cga.reset_index()
+first = list(map(lambda x: x.split('//')[0], cga['index']))
+second = list(map(lambda x: x.split('//')[1], cga['index']))
+
+cga['first'] = first
+cga['second'] = second
 # %%                   Curvas de Localização
 ordered_shares = pd.DataFrame()
 for i in regional_share.columns:
@@ -65,4 +77,57 @@ for i in regional_share.columns:
 
 ordered_shares = ordered_shares.iloc[1:,:]
 
-accumulated_shares = dered_shares.cumsum()
+accumulated_shares = ordered_shares.cumsum()
+# %%
+'''
+            Respostas das atividades
+
+'''
+
+# %%  
+
+'''
+
+        Quociente Locacional para as três regiões onde o setor é relatviamente
+        mais concentrado
+ 
+
+'''
+
+for i in regions:
+    print(quo_loc_reg[i].sort_values(ascending=False).head())
+# %% 
+'''
+        Coeficiente de Localização (valores)
+
+'''
+print(cl[regions])
+ 
+# %% 
+'''
+        Coeficiente Associação Geográfica (valores)
+
+'''
+coef_ass = cga[cga['first'].isin(regions)]
+for i in regions:
+    ranked_coef = coef_ass[coef_ass['first'] == i].sort_values(0, ascending=False)
+    print('\n')
+    print(i)
+    
+    print(ranked_coef.head()[[0, 'second']])
+    
+
+# %%
+'''
+        Curvas de Localização (identificar posição relativa dos setores)
+
+'''
+sorted_curves = pd.DataFrame(accumulated_shares.mean().\
+                             sort_values(ascending=False).dropna()).reset_index()
+    
+sorted_curves.index = sorted_curves.index +1
+    
+curves_ranks = sorted_curves[sorted_curves['index'].isin(regions)]
+
+
+
